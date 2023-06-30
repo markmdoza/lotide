e80dc061c1f2a08a7340baec70aa95cf88986cec
Mark Mendoza <marklmdoza@gmail.com>
2023-06-29 22:41:22 -0400

    Implemented takeUntil.js

--- eqArrays.js
+++ eqArrays.js
@@ -6,19 +6,18 @@ const assertEqual = function (actual, expected) {
   }
 }
 
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
 
-  for (x = 0; x < array1.length; x++) {
-    if (array1[x] !== array2[x])
-    return false; {
-      if (array1[x] === array2[x])
-      return true;
-    }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i])
+      return false;
   }
-}
+
+  return true;
+};
 
 // Takes two arrays
 // Should return True or False based on perfect match
