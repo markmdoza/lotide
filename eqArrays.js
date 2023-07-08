6e5637bf9b65de5245c491a04ab74b4257faac0c
Mark Mendoza <marklmdoza@gmail.com>
2023-07-08 19:32:38 -0400

    Refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🤩 Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`🫠 Assertion Failed: ${actual} !== ${expected}`)
-  }
-}
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -19,11 +13,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-// Takes two arrays
-// Should return True or False based on perfect match
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
