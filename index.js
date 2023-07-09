dd9f5c61323d7ade024cab453ce79e4ef033418c
Mark Mendoza <marklmdoza@gmail.com>
2023-07-09 16:19:33 -0400

    Implemented Object Shorthand

--- index.js
+++ index.js
@@ -18,21 +18,21 @@ const takeUntil = require('./takeUntil');
 const without = require('./without');
 
 module.exports = {
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  middle: middle,
-  head:   head,
-  tail:   tail,
-  takeUntil: takeUntil,
-  without: without
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  middle,
+  head,
+  tail,
+  takeUntil,
+  without
 };
