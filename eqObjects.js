4c0fe7806d0a3673c8413142b55554fe8dedaf1c
Mark Mendoza <marklmdoza@gmail.com>
2023-07-09 16:15:13 -0400

    Exported library

--- eqObjects.js
+++ eqObjects.js
@@ -1,22 +1,5 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]){
-    return false
-    }
-  }
-  return true
-};
-
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🤩 Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`🫠 Assertion Failed: ${actual} !== ${expected}`)
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
 
 const eqObjects = function(object1, object2) {
   const keys1 = Object.keys(object1);
@@ -43,7 +26,7 @@ const eqObjects = function(object1, object2) {
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
 
-// assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+console.log(assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true));
 
 // const shirtObject = { color: "red", size: "medium" };
 // const anotherShirtObject= { size: "medium", color: "red" };
